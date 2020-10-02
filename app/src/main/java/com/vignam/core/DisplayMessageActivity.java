b7c477dac4f6aba7f50e1c21f91cae7ff54c2b4d
Prem kumar Palani <premkumar.nps@gmail.com>
2020-10-02 16:51:02 +0530

    FirstApp code

---
+++ app/src/main/java/com/vignam/core/DisplayMessageActivity.java
@@ -0,0 +1,22 @@
+package com.vignam.core;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView displayTextView = findViewById(R.id.displayMessageText);
+        displayTextView.setText(message);
+    }
+}
